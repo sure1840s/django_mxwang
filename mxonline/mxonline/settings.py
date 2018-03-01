d983e498f82921dfa5c5b029de26d0b548d1bc4e
sure1840s <lllagentina@163.com>
2018-03-01 11:18:03 +0800

    前端页面配置

--- mxonline/mxonline/settings.py
+++ mxonline/mxonline/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'mxonline.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,3 +130,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
