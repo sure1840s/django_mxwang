2ea56228d3d192e6b83d143d5b561a69e2769185
sure1840s <lllagentina@163.com>
2018-03-02 00:19:44 +0800

    user配置

--- mxonline/mxonline/settings.py
+++ mxonline/mxonline/settings.py
@@ -28,6 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTHENTICATION_BACKENDS = (
+    'users.views.CustomBackend',
+)
 
 # Application definition
 
--- mxonline/mxonline/urls.py
+++ mxonline/mxonline/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 import xadmin
 
+from users.views import LoginView
+
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name="index")
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name="index"),
+    url(r'^login/$', LoginView.as_view(), name="login")
 ]
