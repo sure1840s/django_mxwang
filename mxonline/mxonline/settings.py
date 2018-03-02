7c593b358b3cf50956fcfdb9bbd8d18a08c9bc71
sure1840s <lllagentina@163.com>
2018-03-02 22:49:08 +0800

    用户登陆注册和找回密码

--- mxonline/mxonline/settings.py
+++ mxonline/mxonline/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'organization',
     'operation',
     'xadmin',
-    'crispy_forms'
+    'crispy_forms',
+    'captcha'
 ]
 AUTH_USER_MODEL = "users.UserProfile"
 
@@ -136,3 +137,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+EMAIL_HOST = "smtp.sina.com"
+EMAIL_PORT = 25
+EMAIL_HOST_USER = "hassunrain@sina.com"
+EMAIL_HOST_PASSWORD = "admin123"
+EMAIL_USE_TLS = False
+EMAIL_FROM = "hassunrain@sina.com"
